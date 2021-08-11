5ab6210eac6f4cea7320f574134e3462b27bb14e
xy1989 <82069918+xy1989@users.noreply.github.com>
2021-08-11 14:47:11 -0700

    Created database. Keybinding for enter key, that calls function new_task.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.modules import keybinding
 from kivymd.app import MDApp
 
 
@@ -8,6 +10,11 @@ from kivymd.app import MDApp
 class MainApp(MDApp):
     def build(self):
         return Builder.load_file("frontend.kv")
+
+    def new_task(self):
+        print("hello world")
+
+
 
 
 

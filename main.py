7faec03876e50eac67668371724ebe41531ad576
xy1989 <82069918+xy1989@users.noreply.github.com>
2021-08-12 13:33:59 -0700

    Created TextBox Class

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.modules import keybinding
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
 from kivymd.app import MDApp
 from kivymd.uix.behaviors import TouchBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
@@ -12,36 +13,23 @@ from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.list import OneLineIconListItem
 
 
-class Database(MDBoxLayout):
+class TextBox(MDBoxLayout):
     database_path = "todo.db"
-
-    def start_connection(self):
-        """Returns connection to database"""
-        connection = sqlite3.connect(self.database_path)
-        return connection
-
-    def commit_connection(self, connection):
-        """Commit changes and closes connection to database"""
-        self.connection.commit()
-        self.connection.close()
 
     def add_record(self):
         """Adds a new record to the database"""
+
         task = self.ids.input.text  # Stores text input into variable
-        self.connection = self.start_connection()
+
+        self.connection = sqlite3.connect(self.database_path)
         self.connection.execute("""
-        INSERT INTO todo VALUES(?)
-        """, [task])
-        self.commit_connection(self.connection)
-
-    def edit_record(self):
-        pass
-
-    def remove_record(self):
-        pass
+            INSERT INTO todo VALUES(?)
+            """, [task])
+        self.connection.commit()
+        self.connection.close()
 
 
-class ToDoList(MDBoxLayout):
+class TaskList(MDBoxLayout):
     def add_task(self):
         pass
 
@@ -52,14 +40,14 @@ class ToDoList(MDBoxLayout):
         pass
 
 
-class RootWidget(MDBoxLayout):
+class Home(MDBoxLayout):
     pass
 
 
 class MainApp(MDApp):
     def build(self):
         Builder.load_file("frontend.kv")
-        return RootWidget()
+        return Home()
 
 
 MainApp().run()

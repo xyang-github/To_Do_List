8006fe35da5e4b1374edc141ac834ecc35714689
xy1989 <82069918+xy1989@users.noreply.github.com>
2021-08-12 10:00:06 -0700

    Created empty classes

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
+import sqlite3
+
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.modules import keybinding
 from kivymd.app import MDApp
+from kivymd.uix.behaviors import TouchBehavior
+from kivymd.uix.list import OneLineIconListItem
 
 
+class Database:
+    database_path = "todo.db"
+
+    def start_connection(self):
+        """Returns connection to database"""
+        connection = sqlite3.connect(self.database_path)
+        return connection
+
+    def add_record(self):
+        pass
+
+    def edit_record(self):
+        pass
+
+    def remove_record(self):
+        pass
+
+
+class ToDoList(TouchBehavior, OneLineIconListItem):
+    def add_task(self):
+        pass
+
+    def edit_task(self):
+        pass
+
+    def remove_task(self):
+        pass
 
 
 class MainApp(MDApp):
@@ -13,9 +44,6 @@ class MainApp(MDApp):
 
     def new_task(self):
         print("hello world")
-
-
-
 
 
 MainApp().run()

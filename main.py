d1892926b4ff04545a08cfcf08a6373236b047eb
xy1989 <82069918+xy1989@users.noreply.github.com>
2021-08-12 15:24:55 -0700

    Allows submitted task to appear on the list.

--- main.py
+++ main.py
@@ -1,53 +1,63 @@
 import sqlite3
-
-from kivy.app import App
-from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.modules import keybinding
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
 from kivymd.app import MDApp
-from kivymd.uix.behaviors import TouchBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.gridlayout import MDGridLayout
-from kivymd.uix.list import OneLineIconListItem
+from kivymd.uix.list import OneLineIconListItem, IRightBodyTouch, OneLineAvatarIconListItem
+from kivymd.uix.selectioncontrol import MDCheckbox
 
 
-class TextBox(MDBoxLayout):
+class Database:
+    database_path = "todo.db"
+
+    def start_connection(self):
+        connection = sqlite3.connect(self.database_path)
+        return connection
+
+
+class ListItemWithCheckBox(OneLineAvatarIconListItem):
+    """Custom list with an icon as the left widget"""
+    icon = StringProperty("reminder")
+
+
+class RightCheckbox(IRightBodyTouch, MDCheckbox):
+    """Custom right container."""
+
+
+class ToDoList(MDBoxLayout):
     database_path = "todo.db"
 
     def add_record(self):
         """Adds a new record to the database"""
 
-        task = self.ids.input.text  # Stores text input into variable
-
-        self.connection = sqlite3.connect(self.database_path)
+        self.task = self.ids.input.text  # Stores text input into variable
+        self.connection = Database().start_connection()
         self.connection.execute("""
             INSERT INTO todo VALUES(?)
-            """, [task])
+            """, [self.task])
         self.connection.commit()
         self.connection.close()
 
-
-class TaskList(MDBoxLayout):
-    def add_task(self):
-        pass
-
-    def edit_task(self):
-        pass
-
-    def remove_task(self):
-        pass
-
-
-class Home(MDBoxLayout):
-    pass
+        self.ids.scroll_list.add_widget(
+            ListItemWithCheckBox(text=f"{self.task}", icon="reminder"))
+        self.task = self.ids.input.text = ""  # Deletes text from textbox after adding to record
 
 
 class MainApp(MDApp):
     def build(self):
         Builder.load_file("frontend.kv")
-        return Home()
+        return ToDoList()
+
+    def on_start(self):
+        """Need to load the whole database as a list upon start"""
+        connection = Database().start_connection()
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT * FROM todo
+        """)
+        result = cursor.fetchall()
+        connection.close()
+        print(result)  # Work still needs to be done
 
 
 MainApp().run()
